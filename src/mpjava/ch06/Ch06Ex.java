fac4f5755efed2500c9b6b2435a9bcabc4cac389
유경원 <@DESKTOP-QU5R6U9>
2020-04-06 23:50:49 +0900

    complete

--- src/mpjava/ch06/Ch06Ex.java
+++ src/mpjava/ch06/Ch06Ex.java
@@ -65,13 +65,14 @@ class TenSecGame {
 		scan.nextLine();
 		Calendar before = Calendar.getInstance();
 		this.beforeSec = before.get(Calendar.SECOND);
-
 		System.out.println("현재 초 시간 = " + beforeSec);
+
 		System.out.print("10초 예상 후 <Enter>키>>");
 		scan.nextLine();
 		Calendar after = Calendar.getInstance();
 		this.afterSec = after.get(Calendar.SECOND);
 		System.out.println("현재 초 시간 = " + afterSec);
+
 		this.result = Math.abs(beforeSec - afterSec);
 	}
 
@@ -85,17 +86,171 @@ class TenSecGame {
 
 }
 
+class Person {
+	static Scanner scan = new Scanner(System.in);
+	String name;
+	int num[] = new int[3];
+
+	public Person(String name) {
+		this.name = name;
+	}
+
+	public void play() {
+		System.out.print("[" + name + "]:");
+		scan.nextLine();
+		System.out.print("\t");
+		for (int i = 0; i < num.length; i++) {
+			num[i] = (int) (Math.random() * 3) + 1;
+			System.out.print(num[i] + " ");
+		}
+		if (num[0] == num[1] && num[1] == num[2]) {
+			System.out.println(name + "님이 이겼습니다!");
+			System.exit(0);
+		} else {
+			System.out.println("아쉽군요!");
+		}
+
+	}
+
+}
+
 public class Ch06Ex {
 	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		String s;
-		while(true){
+		System.out.print("겜블링 게임에 참여할 선수 숫자>>");
+		Person[] player = new Person[scan.nextInt()];
+		for(int i=0;i<player.length;i++) {
+			System.out.print((i+1)+"번째 선수 이름>>");
+			player[i]=new Person(scan.next());
+		}
+		int i=0;
+		while (true) {
+			player[i].play();
+			i++;
+			if(i==player.length) i=0;
+		}
+	}
+	public static void Ex11(String[] args) {
 		System.out.print(">>");
-		s=scan.nextLine();
-		if(s.equals("그만")) break;
-		StringTokenizer st = new StringTokenizer(s," ");
-		System.out.println("어절 개수는 "+st.countTokens());
+		String s = scan.nextLine();
+		StringBuffer sb = new StringBuffer(s);
+
+		while (true) {
+			System.out.print("명령: ");
+			String com = scan.nextLine();
+			if (com.equals("그만"))
+				break;
+			String[] comL = com.split("!");
+
+			int index = sb.indexOf(comL[0]);
+			if (index == -1) {
+				System.out.println("찾을 수 없습니다!");
+				continue;
+			}
+			sb.replace(index, index + comL[0].length(), comL[1]);
+			System.out.println(sb.toString());
+		}
+		System.out.println("종료합니다");
+
+	}
+
+	public static void Ex10(String[] args) {
+		System.out.print("1번째 선수 이름>>");
+		Person player1 = new Person(scan.next());
+		System.out.print("2번째 선수 이름>>");
+		Person player2 = new Person(scan.next());
+
+		while (true) {
+			player1.play();
+			player2.play();
+		}
+	}
+
+	public static void Ex09(String[] args) {
+		while (true) {
+			System.out.print("철수[가위(1), 바위(2), 보(3), 끝내기(4)]>>");
+			int num = scan.nextInt();
+			if (num == 4)
+				break;
+
+			int com = (int) (Math.random() * 3) + 1;
+			if (num == com) {
+				System.out.println("비겼습니다.");
+				continue;
+			}
+
+			switch (num) {
+			case 1:// 가위
+				switch (com) {
+				case 2:
+					System.out.println("컴퓨터가 이겼습니다.");
+					break;
+				case 3:
+					System.out.println("철수가 이겼습니다.");
+					break;
+				}
+				break;
+			case 2:// 바위
+				switch (com) {
+				case 3:
+					System.out.println("컴퓨터가 이겼습니다.");
+					break;
+				case 1:
+					System.out.println("철수가 이겼습니다.");
+					break;
+				}
+				break;
+			case 3:// 보
+				switch (com) {
+				case 1:
+					System.out.println("컴퓨터가 이겼습니다.");
+					break;
+				case 2:
+					System.out.println("철수가 이겼습니다.");
+					break;
+				}
+				break;
+			}
+		}
+		System.out.println("프로그램 종료");
+	}
+
+	public static void Ex08(String[] args) {
+		System.out.println("문자열을 입력하세요. 빈칸이 있어도 되고 영어 한글 모두 됩니다.");
+		String s = scan.nextLine();
+		StringBuffer sb = new StringBuffer(s);
+		for (int i = 0; i < sb.length(); i++) {
+			sb.append(sb.charAt(0));
+			sb.deleteCharAt(0);
+			System.out.println(sb);
+		}
+
+	}
+
+	public static void Ex07_2(String[] args) {
+		String s;
+		while (true) {
+			System.out.print(">>");
+			s = scan.nextLine();
+			if (s.equals("그만"))
+				break;
+			String[] sl = s.split(" ");
+			System.out.println("어절 개수는 " + sl.length);
+		}
+		System.out.println("종료합니다...");
+
+	}
+
+	public static void Ex07_1(String[] args) {
+		String s;
+		while (true) {
+			System.out.print(">>");
+			s = scan.nextLine();
+			if (s.equals("그만"))
+				break;
+			StringTokenizer st = new StringTokenizer(s, " ");
+			System.out.println("어절 개수는 " + st.countTokens());
 		}
 		System.out.println("종료합니다...");
 	}
@@ -109,17 +264,15 @@ public class Ch06Ex {
 		tsg2.game();
 
 		String winner;
-		winner = (tsg.result == tsg2.result) ? "없음(무승부)"
-				: (tsg.result < tsg2.result) ? tsg.user : tsg2.user;
-		System.out.println(tsg.user + "의 결과 " + tsg.result + ", " + tsg2.user
-				+ "의 결과 " + tsg2.result + ", 승자는 " + winner);
+		winner = (tsg.result == tsg2.result) ? "없음(무승부)" : (tsg.result < tsg2.result) ? tsg.user : tsg2.user;
+		System.out.println(
+				tsg.user + "의 결과 " + tsg.result + ", " + tsg2.user + "의 결과 " + tsg2.result + ", 승자는 " + winner);
 
 	}
 
 	public static void Ex05(String[] args) {
 		Calendar now = Calendar.getInstance();
-		System.out.println("현재 시간은 " + now.get(Calendar.HOUR_OF_DAY) + "시 "
-				+ now.get(Calendar.MINUTE) + "분입니다.");
+		System.out.println("현재 시간은 " + now.get(Calendar.HOUR_OF_DAY) + "시 " + now.get(Calendar.MINUTE) + "분입니다.");
 		int hour = now.get(Calendar.HOUR_OF_DAY);
 		if (4 <= hour && hour < 12) {
 			System.out.println("Good Morning");
